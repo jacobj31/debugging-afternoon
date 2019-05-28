adbaa119419b258e3390c56676c77e93d48e6540
jacobj31 <jespersen.jacobneil@gmail.com>
2019-05-28 12:30:13 -0600

    finished 1, started 2

--- stage-2/src/Components/StoreFront/StoreFront.js
+++ stage-2/src/Components/StoreFront/StoreFront.js
@@ -2,8 +2,11 @@ import React, { Component } from 'react';
 import './StoreFront.css';
 
 class StoreFront extends Component {
+
   render() {
+ 
     let productDisplay = this.props.products.map((element, index) => {
+      
       return (
         <div className="product-container" key={index}>
           <h2>{element.title}</h2>
